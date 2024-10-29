4b794145f00870ba040ee5ed18be624e5f28e85e
carlaaaisidoraa7 <carlayupanquiuss@gmail.com>
2024-10-29 16:22:04 -0300

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -6,7 +6,7 @@ st.sidebar.text('Ingrese un nombre de ususario')
 st.sidebar.text_input('')
 
 st.sidebar.text('cree una contraseña para su usuario')
-st.sidebar.text_input()
+st.sidebar.text_input('')
 globito = st.button('¡Termine de ingresar datos!')
 if globito is True:
     st.balloons() and st.success('hemos terminado el proceso de inicio')
