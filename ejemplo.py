cbd46d30799c470688951a63d4bf4480b7dca507
carlaaaisidoraa7 <carlayupanquiuss@gmail.com>
2024-10-29 16:32:54 -0300

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title('Primera pagina a crear')
 st.header('segundo intento')
 st.header('¡Bienvenido nuevo usuario')
-st.selectbox('Seleccione una opcion, ('Nuevo usuario', 'Tengo ya creada una cuenta'))
+st.selectbox('Seleccione una opcion, ('Nuevo usuario', ('Tengo ya creada una cuenta'))
