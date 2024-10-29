204d0f1647374ede4b4735b6bfde407baf125e53
carlaaaisidoraa7 <carlayupanquiuss@gmail.com>
2024-10-29 16:32:06 -0300

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -1,14 +1,5 @@
 import streamlit as st 
-st.title('intento de codigo uno')
-st.header('intento de primera página')
-
-st.sidebar.text('Ingrese un nombre de ususario')
-st.sidebar.text_input('')
-
-st.sidebar.text('cree una contraseña para su usuario')
-st.sidebar.text_input('')
-globito = st.button('¡Termine de ingresar datos!')
-if globito is True:
-    st.balloons() and st.success('hemos terminado el proceso de inicio')
-else:
-    st.write('ya falta poco tu puedes')
+st.title('Primera pagina a crear')
+st.header('segundo intento')
+st.header('¡Bienvenido nuevo usuario')
+st.selectbox('Seleccione una opcion, ('Nuevo usuario', 'Tengo ya creada una cuenta'))
