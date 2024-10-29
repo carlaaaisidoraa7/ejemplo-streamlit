69e80563d9236871956fc3b0f9652cf8937e2c04
carlaaaisidoraa7 <carlayupanquiuss@gmail.com>
2024-10-29 16:54:28 -0300

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -1,9 +1,14 @@
 import streamlit as st 
 st.title('Primera página a crear')
 st.header('Segundo intento')
+
 st.header('¡Bienvenido nuevo usuario!')
-st.selectbox('Seleccione una opcion', ('Nuevo usuario', 'Tengo ya creada una cuenta'))
+ss=st.selectbox('Seleccione una opcion', ('Nuevo usuario', 'Tengo ya creada una cuenta'))
+
 st.sidebar.write('Espero se sienta emocionado de comersar sesión en esta página')
 st.sidebar.image('gatito.png')
+
 st.sidebar.write('Deberia de iniciar sesión y averiguar que pasa')
 st.sidebar.image('gatobotas.jpg')
+
+print(ss)
