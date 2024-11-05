23433be87a65e6787711b7e36f9f92cc5232fa5d
carlaaaisidoraa7 <carlayupanquiuss@gmail.com>
2024-11-05 17:15:17 -0300

    Update nueva_materia.py

--- nueva_materia.py
+++ nueva_materia.py
@@ -3,10 +3,11 @@ st.title('Primera página a crear')
 st.header('Segundo intento')
 
 st.header('¡Bienvenido nuevo usuario!')
-ss=st.selectbox('Seleccione una opcion', ('Me gustan los gatitos', 'Prefiero los perritos'))
+ss=st.selectbox('Seleccione una opcion', ('Me gustan los gatitos', 'Prefiero los perritos', 'abejita'))
 
 st.sidebar.write('Espero se sienta emocionado de ver sus opciones en esta página')
 st.sidebar.image('gatitito.png')
+st.sidebar.image('abejita{.jpeg')
 
 st.sidebar.write('Deberia seleccionar una función y averiguar que pasa')
 
@@ -14,8 +15,10 @@ st.balloons()
 if ss== 'Me gustan los gatitos':
   st.image('gatito.botitas.jpg')
   st.video('gatitobeso.mp4')
-else:
+elif:
   st.image('perrito.jpeg')
   st.image('perrito2.jpg')
+else:
+  st.audio('')
 st.write(ss)
 
