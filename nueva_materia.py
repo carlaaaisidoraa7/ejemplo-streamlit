4ccd83be9f4f3ed18dcd8a9531822bb77311a7f0
carlaaaisidoraa7 <carlayupanquiuss@gmail.com>
2024-11-05 16:58:10 -0300

    Update nueva_materia.py

--- nueva_materia.py
+++ nueva_materia.py
@@ -1,1 +1,21 @@
+import streamlit as st 
+st.title('Primera página a crear')
+st.header('Segundo intento')
+
+st.header('¡Bienvenido nuevo usuario!')
+ss=st.selectbox('Seleccione una opcion', ('Me gustan los gatitos', 'Prefiero los perritos'))
+
+st.sidebar.write('Espero se sienta emocionado de ver sus opciones en esta página')
+st.sidebar.image('gatitito.png')
+
+st.sidebar.write('Deberia seleccionar una función y averiguar que pasa')
+
+st.balloons()
+if ss== 'Me gustan los gatitos':
+  st.image('gatito.botitas.jpg')
+  st.video('gatitobeso.mp4')
+else:
+  st.image('perrito.jpeg')
+  st.image('perrito2.jpg')
+st.write(ss)
 
